ee85d3e81b71d602f747c82b90c8e30cd63a0267
Julio David Rodríguez Ureche <96896677+KritiusOne@users.noreply.github.com>
2022-02-05 12:21:36 -0500

    Add files via upload
    Cambios a los styles.css, agregado modo claro con js y cambios en el index.html

--- javaScript/script.js
+++ javaScript/script.js
@@ -1,1 +1,23 @@
-alert("hola mundo");
+const button = document.getElementById("switch");
+const moon = document.querySelector(".fa-moon");
+const sun = document.querySelector(".fa-sun");
+const botones = document.querySelectorAll(".button");
+const footer = document.querySelector(".footer");
+const homeImg = document.querySelector(".home__img");
+const input = document.querySelectorAll(".input--contact");
+
+
+button.addEventListener('click', (e)=>{
+    if(e.target === button || e.target === sun || e.target === moon){
+        button.classList.toggle("active");
+        document.body.classList.toggle("sparkly");
+        botones.forEach((e)=>{
+            e.classList.toggle("button--sparkly");
+        })
+        footer.classList.toggle("footer--sparkly");
+        homeImg.classList.toggle("sparkly--img");
+        input.forEach((e)=>{
+            e.classList.toggle("sparkly--input");
+        })
+    }
+})
